252f672caeba2137dca626151250fef7d62809b6
yangyang01@sandboxol.com <lcx@SC-201911281210.localdomain>
2022-07-07 12:07:56 +0800

    第二次画线

--- render/brush/MeshBrush.cpp
+++ render/brush/MeshBrush.cpp
@@ -3,7 +3,7 @@
 void MeshBrush::DrawLine(const Point& a,const Point& b, TGAImage &image, TGAColor color){
     const int xDiff = b.x - a.x;
     const int yDiff = b.y - a.y;
-    for (float i = 0; i<1; i+=0.001) { 
+    for (float i = 0; i<1; i+=0.1) { 
         int x = a.x + xDiff * i; 
         int y = a.y + yDiff * i; 
         image.set(x, y, color); 
